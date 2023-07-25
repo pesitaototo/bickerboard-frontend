94b153a775d63d4fb2e466f4643bc84b509fc325
PE5I <116784810+PE5I@users.noreply.github.com>
2023-07-25 05:43:36 -0700

    update initialize

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import {
   Route,
   Routes
 } from 'react-router-dom';
+import Topic from './components/Topic';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { initializeTopic } from './reducers/topicReducer';
+import { useAppDispatch } from './hooks';
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initializeTopic())
+  }, [dispatch])
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<h1>Test</h1>} />
+        <Route path="/" element={<Topic />} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/signup" element={<h1>Sign up</h1>} />
       </Routes>

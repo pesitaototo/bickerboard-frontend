1fa4236cdf17e9430bdd5724c506c2028e54142d
PE5I <116784810+PE5I@users.noreply.github.com>
2023-07-25 05:45:32 -0700

    update baseUrl

--- src/services/topicService.ts
+++ src/services/topicService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { baseUrl } from "../config";
 import TopicType from "../types/topicType";
 
-const topicBaseUrl = baseUrl + '/api/bickers'
+const topicBaseUrl = baseUrl + '/api/topics'
 
 const getAll = async (): Promise<TopicType[]> => {
   const response = await axios.get(topicBaseUrl);

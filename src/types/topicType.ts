25295c49ca142ec8036dcd8f909568c1193e0513
PE5I <116784810+PE5I@users.noreply.github.com>
2023-08-04 06:04:50 -0700

    update Topic type to include user field

--- src/types/topicType.ts
+++ src/types/topicType.ts
@@ -5,6 +5,15 @@ interface TopicType {
   title: string;
   body: string;
   userId: number;
+  user: {
+    id: number;
+    username: string;
+    email: string;
+    isAdmin: boolean;
+    isEnabled: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
   createdAt: Date;
   updatedAt: Date;
 }

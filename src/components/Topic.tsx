235193bcc79ae7e05bc159bb1062b013c03163b0
PE5I <116784810+PE5I@users.noreply.github.com>
2023-08-04 06:23:34 -0700

    update component

--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -1,13 +1,35 @@
-import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import { useAppSelector } from "../hooks";
+import TopicType from "../types/topicType";
+// import { FC } from "react";
+import { useParams } from "react-router-dom";
 
+// interface TopicProps {
+//   id: number;
+// }
 
 const Topic = () => {
-  const topics = useSelector((state: RootState) => state.topics)
+  const { id } = useParams();
+  const numId = Number(id);
+
+  const topic: TopicType = useAppSelector((state: RootState) => 
+    state.topics.filter(topic => topic.id === numId)
+  )[0]
+
+  if (!topic) return(<>sorry</>);
+
+  const unixTimpstamp = Date.parse(topic.createdAt.toString());
+  const date = new Date(unixTimpstamp * 1000);
+  const day = date.getDay();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getUTCFullYear();
 
   return (
-    <div>
-      {topics.map(topic => topic.title)}
+    <div className="topic-data">
+      <div className="topic-item">{topic.title}</div>
+      <div className="topic-item">{topic.userId}</div>
+      <div className="topic-item">0</div>
+      <div className="topic-last-post">{month} {day}, {year}</div>
     </div>
   );
 }
